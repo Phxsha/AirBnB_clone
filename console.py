2057879ce75119a7ba00a2c6d46b69280eaa3f7e
Phxsha <phashateboho@gmail.com>
2024-03-11 21:38:50 +0200

    My official console

--- console.py
+++ console.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python3
-"""
-Defines the HBNBCommand class, a command interpreter.
-"""
+"""Defines the HBnB console."""
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
     """
-    Command interpreter for HBnB project.
-    """
+
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
-        """
-        Quit command to exit the program.
-        """
-        return True
-
-    def help_quit(self):
-        """
-        Help message for quit command.
-        """
-        print("Quit command to exit the program")
-
-    def do_EOF(self, arg):
-        """
-        Exit the program gracefully on EOF.
-        """
-        print()
-        return True
-
     def emptyline(self):
-        """
-        Do nothing on an empty line.
-        """
+        """Do nothing upon receiving an empty line."""
         pass
 
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
 
-if __name__ == '__main__':
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
-
