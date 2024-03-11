3543eb87e41c045850c21ff8e9aa542b001a862b
Phxsha <phashateboho@gmail.com>
2024-03-11 21:01:58 +0200

    format changes and stuff

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""
+Defines the FileStorage class.
+"""
 import json
 from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
+    """
+    Represent an abstracted storage engine.
 
     Attributes:
         __file_path (str): The name of the file to save objects to.
@@ -20,24 +23,23 @@ class FileStorage:
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[obj_key] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        objects_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+            json.dump(objects_dict, f)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+                obj_dict = json.load(f)
+                for obj_id, obj in obj_dict.items():
+                    cls_name = obj["__class__"]
+                    del obj["__class__"]
+                    self.new(eval(cls_name)(**obj))
         except FileNotFoundError:
             return
